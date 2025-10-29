56cd0d8a968db9158d40e4135d996829b64d1a98
Tanvi-code239 <tanvikumarcutie@gmail.com>
2025-10-29 18:44:11 +0530

    Add files via upload

---
+++ simple program.py
@@ -0,0 +1,19 @@
+# simple calculator.py
+num1_str = input(" enter the first number: ")
+num2_str = input("enter the second number: ")
+a = int(num1_str)
+b = int(num2_str)
+addition = a + b
+subtraction = a - b
+multiplication = a * b
+division = a / b
+print(f"\naddition: {addition}")
+print(f"subtraction: {subtraction}")
+print(f"multiplication: {multiplication}")
+print(f"division: {division})
+
+
+
+
+
+

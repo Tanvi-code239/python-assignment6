a4d380eabfc681153d2e1ff223c576e27e95a3db
Tanvi-code239 <tanvikumarcutie@gmail.com>
2025-10-29 18:45:25 +0530

    Add files via upload

---
+++ simple program2.py
@@ -0,0 +1,5 @@
+# first and last name as input
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+full_name = first_name +" " + last_name
+print(f"hello, {full_name}! welcome to the python program.")
